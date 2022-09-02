dccee06b3fce5e453e6a0b32091484115d18780c
ASUS <ASUS@DESKTOP-825ISI3>
2022-09-02 14:11:23 +0530

    [kms]: my code

---
+++ src/kushal_git/git_code.java
@@ -0,0 +1,21 @@
+package kushal_git;
+
+public class git_code {
+
+	
+	int a =10;
+	int b =50;
+	
+	public  void m1 ()
+	{
+
+		System.out.println(a*b);
+
+	}
+	public static void main(String[] args) {
+		
+		git_code k= new git_code();
+				k.m1();
+	}
+	
+}
